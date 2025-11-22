b924f3f2c9216b838bb078065d3fa7184685cb3e
Balaji26 <balasaran26@gmail.com>
2025-11-22 11:38:56 +0530

    Datatypes 21-11

---
+++ Datatypes.py
@@ -0,0 +1,22 @@
+print("Hello Bala!")
+
+age = input("PLease enter your age")
+print(age) 
+print(type(age))
+
+Phone = input("PLease enter your Phone")
+print(type(Phone))
+print(Phone)
+
+Graduation = input("PLease enter your Graduation")
+print(Graduation)
+
+expected_salary = input("PLease enter your Expected salary")
+print(expected_salary)
+
+age=input("Please enter your age")
+type(age)
+age=int(age)
+age = age+1
+print(age)
+#print is function - it takes a parameter and write to console
---
+++ Yaazhan.py
@@ -0,0 +1,22 @@
+# def read_write():
+#     age = input("enter your age:")
+#     print("Your age is :"+ age)
+    
+# read_write()
+ 
+
+# def read_write(feild):
+#     age = input("enter your"+ feild + ":")
+#     print("Your" + feild + ":"+ age)
+    
+
+# def read_write():
+#     phonenumber = input("enter your Phone number:")
+#     print("Your Phone number is :"+ phonenumber)
+
+def read_write(feild):
+    phonenumber = input("enter your"+ feild + ":")
+    print("Your" + feild + ":"+ phonenumber)
+    
+read_write("phonenumber")
+read_write("age")
