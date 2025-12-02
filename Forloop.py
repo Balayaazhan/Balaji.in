247fd9cc01fd5f13b564bea88c38498ca5618368
Balaji26 <balasaran26@gmail.com>
2025-12-02 11:35:53 +0530

    For_loop exercise.py

--- Forloop.py
+++ Forloop.py
@@ -28,9 +28,9 @@
 #     print("*" * i)
 
 # Print multiples of given number (with muliply by 4)
-i=4
-for i in range (4,21,4):
-    print(i*4)
+# i=1
+# for i in range (4,21,4):
+#     print(i*4)
 
 # print multiplea of given number: 4
 
@@ -52,3 +52,72 @@ for i in range (4,21,4):
 # y = int(input("enter the number"))
 # for odd_number in range (1,y,2):
 #     print(odd_number)
+
+# while exercise:
+
+# i=0
+# while i< 11:
+#     print(i)
+#     i=i+1
+
+# i=0
+# for i in range(0,11,1):
+#     print (i)
+
+# i=10
+# while i<=20:
+#     print(i)
+#     i=i+1
+
+# i= 10
+# while i <=20:
+#     print(i)
+#     i=i+2
+
+# i=20
+# while i >=10:
+#     print(i)
+#     i=i-2
+
+# num=list(range(1,11,1))
+# print(num)
+
+# num=list(range(20,9,-2))
+# print(num)
+
+# num=list(range(50,-1,-5))
+# print(num)
+
+
+# num = list(range(0,31,3))
+# print(num)
+
+# for num in range(0,11,1):
+#     print(num)
+
+# for num in range(1,11,1):
+#     print(num)
+
+
+# for num in range(2,11,2):
+#     print(num)
+
+
+# for num in range(3,31,3):
+#     print(num)
+
+
+# for num in range(20,0,-2):
+#     print(num)
+
+# for num in range(20,0,-1):
+#     print(num)
+
+# for num in range(20,-1,-1):
+#     print(num)
+
+# i= 1
+# i = input("enter the number :")
+# for i in range(1,11,1):
+#     print(i*1,i*2,i*3)
+    
---
+++ for_loopexercise.py
@@ -0,0 +1,41 @@
+ num=list(range(1,11,1))
+# print(num)
+
+# num=list(range(20,9,-2))
+# print(num)
+
+# num=list(range(50,-1,-5))
+# print(num)
+
+
+# num = list(range(0,31,3))
+# print(num)
+
+# for num in range(0,11,1):
+#     print(num)
+
+# for num in range(1,11,1):
+#     print(num)
+
+
+# for num in range(2,11,2):
+#     print(num)
+
+
+# for num in range(3,31,3):
+#     print(num)
+
+
+# for num in range(20,0,-2):
+#     print(num)
+
+# for num in range(20,0,-1):
+#     print(num)
+
+# for num in range(20,-1,-1):
+#     print(num)
+
+# i= 1
+# i = input("enter the number :")
+# for i in range(1,11,1):
+#     print(i*1,i*2,i*3)
