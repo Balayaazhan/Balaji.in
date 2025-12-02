d9c2918d967c38d1841ec4ebde888d9d8ac2c604
Balaji26 <balasaran26@gmail.com>
2025-12-02 10:06:02 +0530

    For_loop.py

---
+++ Forloop.py
@@ -0,0 +1,54 @@
+#def forloop
+
+# i = int(input("Enter num :"))
+# while i<=num:
+#     print("* "* num)
+#     i=i+1
+
+
+# for i in range(1,6):
+#     print("*"* i)
+
+
+# # decrease the star one by one in each row and column
+
+# row = input("enter the ")
+
+# star printing in accending order:
+
+# i=1
+# for i in range(1,6):
+#     print("*" * i)
+
+# star printing in decending order:
+
+# i=5
+
+# for i in range(5,0,-1):
+#     print("*" * i)
+
+# Print multiples of given number (with muliply by 4)
+i=4
+for i in range (4,21,4):
+    print(i*4)
+
+# print multiplea of given number: 4
+
+# i=int(input("enter the number :"))
+# for i in range (i,16,i):
+#     print(i)
+
+#print even number with given range:
+# even_number = 2
+# for even_number in range (2,11,2):
+#     print(even_number)
+
+# print even number less than given number:
+# y = int(input("enter the number"))
+# for even_number in range (2,y,2):
+#     print(even_number)
+
+#print odd number less than given number:
+# y = int(input("enter the number"))
+# for odd_number in range (1,y,2):
+#     print(odd_number)
